33b62350e18e5029fb80ef530a1f498d6babf05e
jacob6707 <22101656+jacob6707@users.noreply.github.com>
2024-03-31 00:21:41 +0100

    Fix a bug, add getting current user

--- models/user.js
+++ models/user.js
@@ -1,54 +1,58 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new Schema(
-  {
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    displayName: {
-      type: String,
-      default: this.username,
-    },
-    avatarUrl: {
-      type: String,
-      default: '/images/default.png',
-    },
-    status: String,
-    about: String,
-    birthday: Date,
-    friends: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: 'Friend',
-      },
-    ],
-    directMessages: [
-      {
-        _id: false,
-        userId: {
-          type: Schema.Types.ObjectId,
-          ref: 'User',
-        },
-        channelId: {
-          type: Schema.Types.ObjectId,
-          ref: 'Channel',
-        },
-      },
-    ],
-  },
-  { timestamps: true }
+	{
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		displayName: {
+			type: String,
+		},
+		avatarUrl: {
+			type: String,
+			default: "/images/default.png",
+		},
+		status: String,
+		about: String,
+		birthday: Date,
+		friends: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: "Friend",
+			},
+		],
+		directMessages: [
+			{
+				_id: false,
+				userId: {
+					type: Schema.Types.ObjectId,
+					ref: "User",
+				},
+				channelId: {
+					type: Schema.Types.ObjectId,
+					ref: "Channel",
+				},
+			},
+		],
+	},
+	{ timestamps: true }
 );
 
-module.exports = mongoose.model('User', userSchema);
+userSchema.pre("save", function (next) {
+	if (!this.displayName) this.displayName = this.get("username");
+	next();
+});
+
+module.exports = mongoose.model("User", userSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,19 @@
-const express = require('express');
-const { body } = require('express-validator');
+const express = require("express");
+const { body } = require("express-validator");
 
-const usersController = require('../controllers/users');
-const isAuth = require('../middleware/is-auth');
+const usersController = require("../controllers/users");
+const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
 
-router.get('/:id', isAuth, usersController.getUser);
+router.get("/", isAuth, usersController.getCurrentUser);
 
-router.post('/:id/add', isAuth, usersController.addFriend);
+router.get("/:id", isAuth, usersController.getUser);
 
-router.delete('/:id/remove', isAuth, usersController.removeFriend);
+router.post("/:id/add", isAuth, usersController.addFriend);
 
-router.post('/:id/message', isAuth, usersController.postMessage);
+router.delete("/:id/remove", isAuth, usersController.removeFriend);
+
+router.post("/:id/message", isAuth, usersController.postMessage);
 
 module.exports = router;
